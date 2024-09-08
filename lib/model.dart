07e5507c3485bbd7a28cb65f1e50428e9f00d1c7
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-09-08 04:07:45 +0100

    :ambulance: create user and st :clown_face: atus and task model :tada:

---
+++ lib/model.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class User {
+  String name;
+  String email;
+  Status status;
+  String balance;
+  List<Task> tasks;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.status,
+    required this.tasks,
+    required this.balance,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      name: json['name'],
+      email: json['email'],
+      balance: json['balance'],
+      status: Status.fromJson(json['status']),
+      tasks: List<Task>.from(json['tasks'].map((task) => Task.fromJson(task))),
+    );
+  }
+}
+
+class Status {
+  int pendingTasks;
+  int completedTasks;
+  int totalTasksTaken;
+
+  Status({
+    required this.pendingTasks,
+    required this.completedTasks,
+    required this.totalTasksTaken,
+  });
+
+  factory Status.fromJson(Map<String, dynamic> json) {
+    return Status(
+      pendingTasks: json['pendingTasks'],
+      completedTasks: json['completedTasks'],
+      totalTasksTaken: json['totalTasksTaken'],
+    );
+  }
+}
+
+class Task {
+  int id;
+  String title;
+  double amount;
+  String description;
+
+  Task({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.description,
+  });
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      amount: json['amount'],
+      description: json['description'],
+    );
+  }
+}
+
+User defaultUser = User(
+  name: '',
+  email: '',
+  balance: '',
+  tasks: [],
+  status: Status(pendingTasks: 0, completedTasks: 0, totalTasksTaken: 0),
+);
+
+Future<User> userData() async {
+  User user = defaultUser;
+  try {
+    final response = await http.post(
+      Uri.parse('http://127.0.0.1:8000/getuser/'),
+      headers: {"Content-Type": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      user = User.fromJson(data);
+    } else {
+      print('Failed: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+  return user;
+}
+
+Future<List<Task>> userTasksData() async {
+  List<Task> tasks = [];
+  try {
+    final response = await http.post(
+      Uri.parse('http://127.0.0.1:8000/getuser/'),
+      headers: {"Content-Type": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      tasks = List<Task>.from(data['tasks'].map((task) => Task.fromJson(task)));
+    } else {
+      print('Failed: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+  return tasks;
+}
